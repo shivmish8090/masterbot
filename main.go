fa30e9c60d893994614011033a761188a52e3ead
विवेक कुमार <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-03-30 11:33:44 +0530

    Update main.go

--- main.go
+++ main.go
@@ -53,7 +53,7 @@ func main() {
 
 // start introduces the bot.
 func start(b *gotgbot.Bot, ctx *ext.Context) error {
-	b.SendPhoto(ctx.EffectiveChat.Id, config.StartImage)
+	b.SendPhoto(ctx.EffectiveChat.Id, config.StartImage, nil)
 	_, err := ctx.EffectiveMessage.Reply(b, fmt.Sprintf("Hello, I'm @%s.\nI am a sample bot", b.User.Username), &gotgbot.SendMessageOpts{
 		ParseMode: "HTML",
 	})
