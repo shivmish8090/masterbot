f14280b31efe9455792aae3aeca951235f935f3f
विवेक कुमार <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-03-30 10:14:41 +0530

    Create config/config.go

---
+++ config/config.go
@@ -0,0 +1,27 @@
+package config
+
+import (
+        "os"
+        "strconv"
+
+        "github.com/joho/godotenv"
+)
+
+var (
+        BotToken      string
+        StartImageUrl string
+)
+
+func init() {
+        godotenv.Load()
+
+        BotToken = Getenv("BOT_TOKEN", "")
+        StartImageUrl = Getenv("START_IMG_URL", "https://graph.org/file/f3c8291963a053ac18536-3558d077ad80845bd7.jpg")
+}
+
+func Getenv(key, defaultValue string) string {
+        if value, exists := os.LookupEnv(key); exists {
+                return value
+        }
+        return defaultValue
+}
