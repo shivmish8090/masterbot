1efeefb06ec60f5e73c62c2bb7e4c6e93b8eb6d7
विवेक कुमार <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-03-30 13:54:54 +0530

    Update main.go

--- main.go
+++ main.go
@@ -110,6 +110,7 @@ To use my features, please upgrade this group to a supergroup.
 2. Tap on "Chat History" and set it to "Visible".
 3. Re-add me, and I'll be ready to help!`
 	}
-	ctx.EffectiveChat.Reply(b, message)
+ctx.EffectiveChat.Reply(b, message, nil)
+ctx.EffectiveChat.Leave(b, nil)
 	return nil
 }
