ad2ed6965d34daa957e21968989afb127cffd6a0
विवेक कुमार <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-03-31 08:35:24 +0530

    Update main.go

--- main.go
+++ main.go
@@ -207,7 +207,7 @@ Let me know if you need any help.`, b.User.FirstName)
 
 func deleteLongMessage(b *gotgbot.Bot, ctx *ext.Context) error {
 	ctx.EffectiveMessage.Delete(b, nil)
-	ctx.EffectiveMessage.Reply(b, "You xan send longer message then 800 characters", nil)
+b.SendMessage(ctx.EffectiveChat.Id, "You xan send longer message then 800 characters", nil)
 	return ext.EndGroups
 }
 
