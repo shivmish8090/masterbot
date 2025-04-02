aa5fa0d33b7a2833bf8cfd550dade69cbf454eff
विवेक कुमार <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-04-02 18:02:53 +0530

    Create echo.go

---
+++ echo.go
@@ -0,0 +1,48 @@
+pacakge main
+
+
+
+func EcoHandler(b *gotgbot.Bot, ctx *ext.Context) error {
+	if ctx.EffectiveChat.Type != "supergroup" {
+		ctx.EffectiveMessage.Reply(
+			b,
+			"This command can be used only in groups",
+			nil,
+		)
+		return nil
+	}
+	if len(ctx.Args()) < 2 {
+		ctx.EffectiveMessage.Reply(b, "Usage: /echo <long message>", nil)
+		return nil
+	}
+
+	ctx.EffectiveMessage.Delete(b, nil)
+	if len(ctx.EffectiveMessage.GetText()) < 500 {
+		b.SendMessage(
+			ctx.EffectiveChat.Id,
+			"Oops! Your message is under 500 characters. You can send it without using /echo.",
+			nil,
+		)
+		return nil
+	}
+
+	text := strings.SplitN(ctx.EffectiveMessage.GetText(), " ", 2)[1]
+	url, err := telegraph.CreatePage(text, ctx.EffectiveUser.Username)
+	if err != nil {
+		return err
+	}
+	if ctx.EffectiveMessage.ReplyToMessage != nil {
+		b.SendMessage(
+			ctx.EffectiveChat.Id,
+			url,
+			&gotgbot.SendMessageOpts{
+				ReplyParameters: &gotgbot.ReplyParameters{
+					MessageId: ctx.EffectiveMessage.ReplyToMessage.MessageId,
+				},
+			},
+		)
+	} else {
+		b.SendMessage(ctx.EffectiveChat.Id, url, nil)
+	}
+	return nil
+}
