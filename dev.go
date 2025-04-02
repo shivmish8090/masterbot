4d9a149ba3858f3435999f9a2acb16f259f3dc24
विवेक कुमार <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-04-02 20:21:08 +0530

    Update dev.go

--- dev.go
+++ dev.go
@@ -38,20 +38,27 @@ func EvalHandler(b *gotgbot.Bot, ctx *ext.Context) error {
 }
 
 func resolveImports(code string) (string, []string) {
-	var imports []string
-	importsRegex := regexp.MustCompile(`import\s*\(([\s\S]*?)\)|import\s*\"([\s\S]*?)\"`)
-	importsMatches := importsRegex.FindAllStringSubmatch(code, -1)
-	for _, v := range importsMatches {
-		if v[1] != "" {
-			imports = append(imports, v[1])
-		} else {
-			imports = append(imports, v[2])
-		}
-	}
-	code = importsRegex.ReplaceAllString(code, "")
-	return code, imports
-}
+    var imports []string
+    importsRegex := regexp.MustCompile(`import\s*([\s\S]*?)|import\s*"([^"]+)"`)
+    importsMatches := importsRegex.FindAllStringSubmatch(code, -1)
 
+    for _, v := range importsMatches {
+        if v[1] != "" { // Multi-line import
+            lines := strings.Split(v[1], "\n")
+            for _, line := range lines {
+                cleanLine := strings.TrimSpace(line)
+                if cleanLine != "" {
+                    imports = append(imports, fmt.Sprintf("%q", cleanLine))
+                }
+            }
+        } else if v[2] != "" { // Single-line import
+            imports = append(imports, fmt.Sprintf("%q", v[2]))
+        }
+    }
+
+    code = importsRegex.ReplaceAllString(code, "")
+    return code, imports
+}
 func runGoCode(code string, imports []string, ctxString string) (string, error) {
 	var importBlock string
 	if len(imports) > 0 {
