7b4a49568cdffb58d22dce7a530bbc7d29a6b076
विवेक कुमार <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-04-02 14:17:55 +0530

    Update filters.go

--- filters/filters.go
+++ filters/filters.go
@@ -82,6 +82,6 @@ func Command(cmd string) func(m *gotgbot.Message) bool {
 	}
 }
 
-func IsLongMessage(m *gotgbot.Message) bool {
+func LongMessage(m *gotgbot.Message) bool {
 	return !ChatAdmin(m) && len(m.GetText()) > 800
 }
