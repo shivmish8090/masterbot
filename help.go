92d7ffc660e4ec0ee737f0a2c4f4fe9202455bf4
विवेक कुमार <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-04-03 07:26:49 +0530

    Update help.go

--- help.go
+++ help.go
@@ -8,21 +8,21 @@ import (
 )
 
 func helpCB(b *gotgbot.Bot, ctx *ext.Context) error {
-	keyboard := gotgbot.InlineKeyboardMarkup{
-		InlineKeyboard: [][]gotgbot.InlineKeyboardButton{
-			{
-				{
-					Text:         "Back",
-					CallbackData: "start_callback",
-				},
-				{
-					Text:         "Close",
-					CallbackData: "close",
-				},
-			},
-		},
-	}
-	_, _, err := ctx.Message.EditCaption(b, &gotgbot.EditMessageCaptionOpts{Caption: "Soon...", ReplyMarkup: keyboard})
-	log.Println(err)
-	return nil
+        keyboard := gotgbot.InlineKeyboardMarkup{
+                InlineKeyboard: [][]gotgbot.InlineKeyboardButton{
+                        {
+                                {Text: "Back", CallbackData: "start_callback"},
+                                {Text: "Close", CallbackData: "close"},
+                        },
+                },
+        }
+
+        _, _, err := ctx.CallbackQuery.Message.EditCaption(b, &gotgbot.EditMessageCaptionOpts{
+                Caption: "Soon...",
+                ReplyMarkup: keyboard,
+        })
+        if err != nil {
+                log.Println("Failed to edit caption:", err)
+        }
+        return nil
 }
