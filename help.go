50d9dd8a1569d7c7e235c093b2814a3eaa924550
विवेक कुमार <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-04-03 06:37:54 +0530

    Create help.go

---
+++ help.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+        "fmt"
+
+        "github.com/PaulSonOfLars/gotgbot/v2"
+        "github.com/PaulSonOfLars/gotgbot/v2/ext"
+
+        "github.com/Vivekkumar-IN/EditguardianBot/config"
+)
+
+func helpCB(b *gotgbot.Bot, ctx *ext.Context) error {
+return nil
+
+
+}
