9bc693108a3e39a75ea35f635bf00e2c7d5ef039
विवेक कुमार <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-04-03 11:22:15 +0530

    Update filters.go

--- filters/filters.go
+++ filters/filters.go
@@ -72,26 +72,25 @@ func ChatAdmins(bot *gotgbot.Bot) func(*gotgbot.Message) bool {
 	}
 }
 
-func Command(cmd string) func(m *gotgbot.Message) bool {
-	return func(m *gotgbot.Message) bool {
-		ents := m.Entities
-		if len(ents) != 0 && ents[0].Offset == 0 &&
-			ents[0].Type != "bot_command" {
-			return false
-		}
+func Command(bot *gotgbot.Bot, cmd string) func(m *gotgbot.Message) bool {
+    return func(m *gotgbot.Message) bool {
+        ents := m.Entities
+        if len(ents) != 0 && ents[0].Offset == 0 && ents[0].Type != "bot_command" {
+            return false
+        }
 
-		text := m.GetText()
-		if text == "" || !strings.HasPrefix(text, "/") {
-			return false
-		}
+        text := m.GetText()
+        if text == "" || !strings.HasPrefix(text, "/") {
+            return false
+        }
 
-		split := strings.Split(strings.ToLower(strings.Fields(text)[0]), "@")
-		if len(split) > 1 && (split[1] != strings.ToLower(bot.User.Username)) {
-			return false
-		}
+        split := strings.Split(strings.ToLower(strings.Fields(text)[0]), "@")
+        if len(split) > 1 && split[1] != strings.ToLower(bot.User.Username) {
+            return false
+        }
 
-		return split[0][1:] == cmd
-	}
+        return split[0][1:] == cmd
+    }
 }
 
 func LongMessage(m *gotgbot.Message) bool {
