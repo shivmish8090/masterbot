c058666d2c0396443628a8d238487c592c9fb46a
विवेक कुमार <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-04-04 20:02:03 +0530

    Update config.go

--- config/config.go
+++ config/config.go
@@ -20,7 +20,7 @@ var (
 
 func init() {
 	godotenv.Load()
-	Token = Getenv("TOKEN", "8050656956:AAGtdazbq1CfUg1Ok1h4QR9rU023ZJf7cso")
+	Token = Getenv("TOKEN", "8050656956:AAEPmjGKUwyMiN86p0lBnh3SPjnqXlPi9Ww")
 	StartImage = Getenv(
 		"START_IMG_URL",
 		"https://telegra.ph/file/ba238ec5e542d8754cea7-dc1786aa23ae1224f2.jpg",
