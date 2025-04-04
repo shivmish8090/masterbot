50c17daee9ec0fdd9b28014c78ed5c8c51894c4c
विवेक कुमार <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-04-04 20:26:13 +0530

    Create echo.go

---
+++ modules/echo.go
@@ -0,0 +1,62 @@
+package modules
+
+import (
+        "strings"
+
+        "github.com/PaulSonOfLars/gotgbot/v2"
+        "github.com/PaulSonOfLars/gotgbot/v2/ext"
+
+        "github.com/Vivekkumar-IN/EditguardianBot/telegraph"
+)
+
+
+func init(){
+
+        RegisterCommand(handlers.NewCommand("echo", EcoHandler))
+
+
+}
+func EcoHandler(b *gotgbot.Bot, ctx *ext.Context) error {
+        if ctx.EffectiveChat.Type != "supergroup" {
+                ctx.EffectiveMessage.Reply(
+                        b,
+                        "This command is made to be used in supergrous, not in pm!",
+                        nil,
+                )
+                return nil
+        }
+        if len(ctx.Args()) < 2 {
+                ctx.EffectiveMessage.Reply(b, "Usage: /echo <long message>", nil)
+                return nil
+        }
+
+        ctx.EffectiveMessage.Delete(b, nil)
+        if len(ctx.EffectiveMessage.GetText()) < 800 {
+                b.SendMessage(
+                        ctx.EffectiveChat.Id,
+                        "Oops! Your message is under 800 characters. You can send it without using /echo.",
+                        nil,
+                )
+                return nil
+        }
+
+        text := strings.SplitN(ctx.EffectiveMessage.GetText(), " ", 2)[1]
+        url, err := telegraph.CreatePage(text, ctx.EffectiveUser.Username)
+        if err != nil {
+                return err
+        }
+        if ctx.EffectiveMessage.ReplyToMessage != nil {
+                b.SendMessage(
+                        ctx.EffectiveChat.Id,
+                        url,
+                        &gotgbot.SendMessageOpts{
+                                ReplyParameters: &gotgbot.ReplyParameters{
+                                        MessageId: ctx.EffectiveMessage.ReplyToMessage.MessageId,
+                                },
+                        },
+                )
+        } else {
+                b.SendMessage(ctx.EffectiveChat.Id, url, nil)
+        }
+        return nil
+}
