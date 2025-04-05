7ec333d2ce845f4024e7b7fdcc2178ce3d2647b7
विवेक कुमार <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-04-05 17:50:27 +0530

    Update editmode.go

--- modules/editmode.go
+++ modules/editmode.go
@@ -7,8 +7,8 @@ import (
 )
 
 func init() {
-	Register(handlers.NewCommand("editmode", EditMode))
-	AddHelp("✍️ Edit Mode", "help_editmode", `<b>✍️ Edit Mode</b>
+        Register(handlers.NewCommand("editmode", EditMode))
+    AddHelp("✍️ Edit Mode", "help_editmode", `<b>✍️ Edit Mode</b>
 
 <b>Command:</b> <code>/editmode &lt;off|admin|user&gt;</code>
 
@@ -18,8 +18,8 @@ Control how the bot handles <b>edited messages</b> in the group.
 <b>Options:</b>
 • <b>/editmode off</b> - Bot won't delete any edited messages.  
 • <b>/editmode user</b> - Deletes edited messages from <b>normal users</b>. (default)  
-• <b>/editmode admin</b> - Deletes edited messages from <b>both users and admins</b>. <i>(Only group owner can set this)</i>`, nil)
-
+• <b>/editmode admin</b> - Deletes edited messages from <b>both users and admins</b>. <i>(Only group owner can set this)</i>`)
+}
 func EditMode(b *gotgbot.Bot, ctx *ext.Context) error {
 	ctx.EffectiveMessage.Reply(b, "Soon..", nil)
 	return nil
