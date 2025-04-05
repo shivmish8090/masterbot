80297981af143029e8fbb90d9cd1bb0b8da27204
विवेक कुमार <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-04-05 06:08:36 +0530

    Update modules.go

--- modules/modules.go
+++ modules/modules.go
@@ -2,20 +2,9 @@ package modules
 
 import "github.com/PaulSonOfLars/gotgbot/v2/ext/handlers"
 
-var (
-	CommandHandlers       []handlers.Command
-	MessageHandlers       []handlers.Message
-	CallbackQueryHandlers []handlers.CallbackQuery
-)
+var Handlers []handlers.Handler
 
-func RegisterCommand(h handlers.Command) {
-	CommandHandlers = append(CommandHandlers, h)
-}
 
-func RegisterMessage(h handlers.Message) {
-	MessageHandlers = append(MessageHandlers, h)
-}
-
-func RegisterCallback(h handlers.CallbackQuery) {
-	CallbackQueryHandlers = append(CallbackQueryHandlers, h)
+func Register(h handlers.Handler) {
+	 Handlers = append(Handlers, h)
 }
