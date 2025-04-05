95974bc73890e9c1a459290170b587a1c4610a66
विवेक कुमार <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-04-05 17:11:49 +0530

    Update editmode.go

--- modules/editmode.go
+++ modules/editmode.go
@@ -5,6 +5,11 @@ import (
 	"github.com/PaulSonOfLars/gotgbot/v2/ext"
 )
 
+func init() {
+        Register(handlers.NewCommand("editmode", EditMode))
+
+}
+
 func EditMode(b *gotgbot.Bot, ctx *ext.Context) error {
 	return nil
 }
