e2b1728ce4d2c3dca1c542917fa2286bc34e9e07
विवेक कुमार <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-04-05 20:03:26 +0530

    Update start.go

--- modules/start.go
+++ modules/start.go
@@ -26,6 +26,9 @@ func start(b *gotgbot.Bot, ctx *ext.Context) error {
 		if !isCallback {
 			ctx.EffectiveMessage.Delete(b, nil)
 		}
+fmt.Println("statt:", ctx.Args())
+fmt.Println("Len", len(ctx.Args())
+fmt.Println(ctx.EffectiveMessage.Text)
 		if len(ctx.Args()) > 2 {
 			modName := ctx.Args()[1]
 			helpString := GetHelp(modName)
