6bdc29a78ed50b2e8125f6ae4a45ce10d6bb2a97
विवेक कुमार <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-04-05 07:09:12 +0530

    Update modules.go

--- modules/modules.go
+++ modules/modules.go
@@ -3,7 +3,22 @@ package modules
 import "github.com/PaulSonOfLars/gotgbot/v2/ext"
 
 var Handlers []ext.Handler
+var ModulesHelp = map[string]struct {
+	Callback string
+	Help     string
+}{}
 
 func Register(h ext.Handler) {
 	Handlers = append(Handlers, h)
+}
+
+
+func AddHelp(name string, callback string, help string) {
+	ModulesHelp[name] = struct {
+		Callback string
+		Help     string
+	}{
+		Callback: callback,
+		Help:     help,
+	}
 }
