7b64e6e63e0d74fe59a547b973908b3cb8b4a15c
विवेक कुमार <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-04-05 09:54:29 +0530

    Update modules.go

--- modules/modules.go
+++ modules/modules.go
@@ -1,24 +1,41 @@
 package modules
 
 import (
+	"log"
+
 	"github.com/PaulSonOfLars/gotgbot/v2/ext"
 	"github.com/PaulSonOfLars/gotgbot/v2/ext/handlers"
 	"github.com/PaulSonOfLars/gotgbot/v2/ext/handlers/filters/callbackquery"
 )
 
+const (
+	MaxHandlers   = 10 // <- Change this to increase/decrease handler limit
+	MaxHelpModules = 10 // <- Change this to increase/decrease help module limit
+)
+
 var (
-	Handlers    []ext.Handler
-	ModulesHelp = map[string]struct {
+	Handlers = make([]ext.Handler, 0, MaxHandlers)
+	ModulesHelp = make(map[string]struct {
 		Callback string
 		Help     string
-	}{}
+	}, MaxHelpModules)
 )
 
 func Register(h ext.Handler) {
+	if len(Handlers) >= MaxHandlers {
+		log.Panic("handler limit exceeded")
+	}
 	Handlers = append(Handlers, h)
 }
 
 func AddHelp(name, callback, help string, h ext.Handler) {
+	if len(ModulesHelp) >= MaxHelpModules {
+		log.Panic("help modules limit exceeded")
+	}
+	if h == nil {
+		h = handlers.NewCallback(callbackquery.Equal(callback), helpModuleCB)
+	}
+	Register(h)
 	ModulesHelp[name] = struct {
 		Callback string
 		Help     string
@@ -26,11 +43,6 @@ func AddHelp(name, callback, help string, h ext.Handler) {
 		Callback: callback,
 		Help:     help,
 	}
-
-	if h == nil {
-		h = handlers.NewCallback(callbackquery.Equal(callback), helpModuleCB)
-	}
-	Register(h)
 }
 
 func init() {
