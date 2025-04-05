dccfeb1bb56c7abb587f7fe0c7ad234428d6153c
विवेक कुमार <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-04-05 17:39:08 +0530

    Update editmode.go

--- modules/editmode.go
+++ modules/editmode.go
@@ -8,7 +8,20 @@ import (
 
 func init() {
 	Register(handlers.NewCommand("editmode", EditMode))
-	AddHelp("✍️ Edit Mode", "help_editmode", "...", nil)
+	AddHelp("✍️ Edit Mode", "help_editmode", `<b>✍️ Edit Mode</b>
+
+<b>Command:</b> <code>/editmode &lt;off|admin|user&gt;</code>
+
+<b>Description:</b>
+Control how the bot handles <b>edited messages</b> in the group.
+
+<b>Options:</b>
+• <code>/editmode off</code> - Bot won't delete any edited messages.  
+• <code>/editmode user</code> - Deletes edited messages from <b>normal users</b>. (default)  
+• <code>/editmode admin</code> - Deletes edited messages from <b>both users and admins</b>.
+
+<b>Note:</b>
+Use this to prevent spam edits or maintain message consistency in the group.`, nil)
 }
 
 func EditMode(b *gotgbot.Bot, ctx *ext.Context) error {
