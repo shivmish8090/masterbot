43263b5684b53ea094ca5b627ba0bc2f122ddf35
विवेक कुमार <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-04-06 19:29:51 +0530

    Update database.go

--- database/database.go
+++ database/database.go
@@ -24,47 +24,60 @@ var (
 var cache sync.Map
 
 func init() {
-	if config.MongoUri == "" {
-		log.Panic("MongoDB URI is missing in config.MongoUri")
-	}
+        if config.MongoUri == "" {
+                log.Panic("MongoDB URI is missing in config.MongoUri")
+        }
 
-	ctx, cancel := context.WithTimeout(context.Background(), timeout)
-	defer cancel()
+        ctx, cancel := context.WithTimeout(context.Background(), timeout)
+        defer cancel()
 
-	var err error
-	client, err = mongo.Connect(options.Client().ApplyURI(config.MongoUri))
-	if err != nil {
-		log.Panicf("Failed to connect to MongoDB: %v", err)
-	}
+        var err error
+        client, err = mongo.Connect(options.Client().ApplyURI(config.MongoUri))
+        if err != nil {
+                log.Panicf("Failed to connect to MongoDB: %v", err)
+        }
 
-	db := client.Database("EditGuardainBot")
-	userDB = db.Collection("userstats")
-	chatDB = db.Collection("chats")
-	editModeDB = db.Collection("editmodes")
-	echoDB = db.Collection("echos")
+        db := client.Database("EditGuardainBot")
+        userDB = db.Collection("userstats")
+        chatDB = db.Collection("chats")
+        editModeDB = db.Collection("editmodes")
+        echoDB = db.Collection("echos")
 
-	_, err = userDB.Indexes().CreateOne(ctx, mongo.IndexModel{Keys: bson.M{"user_id": 1}})
-	if err != nil {
-		log.Printf("Failed to create index on userstats: %v", err)
-	}
+        // Index on userstats.user_id
+        _, err = userDB.Indexes().CreateOne(ctx, mongo.IndexModel{
+                Keys:    bson.M{"user_id": 1},
+                Options: options.Index().SetName("user_id_index"),
+        })
+        if err != nil {
+                log.Printf("Failed to create index on userstats: %v", err)
+        }
 
-	_, err = chatDB.Indexes().CreateOne(ctx, mongo.IndexModel{Keys: bson.M{"chat_id": 1}})
-	if err != nil {
-		log.Printf("Failed to create index on chats: %v", err)
-	}
-	_, err = editModeDB.Indexes().CreateOne(ctx, mongo.IndexModel{
-		Keys:    bson.M{"chat_id": 1},
-		Options: options.Index().SetUnique(true),
-	})
-	if err != nil {
-		log.Printf("Failed to create index on editmodes: %v", err)
-	}
-	_, err = echoDB.Indexes().CreateOne(ctx, mongo.IndexModel{
-		Keys: bson.M{"chat_id": 1},
-	})
-	if err != nil {
-		log.Printf("Failed to create index on echos: %v", err)
-	}
+        // Index on chats.chat_id
+        _, err = chatDB.Indexes().CreateOne(ctx, mongo.IndexModel{
+                Keys:    bson.M{"chat_id": 1},
+                Options: options.Index().SetName("chat_id_index"),
+        })
+        if err != nil {
+                log.Printf("Failed to create index on chats: %v", err)
+        }
+
+        // Unique index on editmodes.chat_id
+        _, err = editModeDB.Indexes().CreateOne(ctx, mongo.IndexModel{
+                Keys:    bson.M{"chat_id": 1},
+                Options: options.Index().SetName("unique_chat_id_editmode").SetUnique(true),
+        })
+        if err != nil {
+                log.Printf("Failed to create unique index on editmodes: %v", err)
+        }
+
+        // Index on echos.chat_id
+        _, err = echoDB.Indexes().CreateOne(ctx, mongo.IndexModel{
+                Keys:    bson.M{"chat_id": 1},
+                Options: options.Index().SetName("chat_id_echo_index"),
+        })
+        if err != nil {
+                log.Printf("Failed to create index on echos: %v", err)
+        }
 }
 
 func Disconnect() {
