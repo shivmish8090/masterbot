82ee22cc6113ce91dccee4e6b60b1bed772f7682
विवेक कुमार <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-04-06 19:49:33 +0530

    Update args.go

--- utils/args.go
+++ utils/args.go
@@ -4,7 +4,7 @@ import (
 	"strings"
 )
 
-func ParseValues(keys []string, args ...string) map[string]string {
+func ParseArgs(keys []string, args ...string) map[string]string {
 	values := make(map[string]string)
 	keySet := make(map[string]struct{})
 	unmatchedKeys := []string{}
