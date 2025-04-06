5c9b0f673bb618419d1d2e071b70c5c174bfb223
विवेक कुमार <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-04-06 13:42:20 +0530

    update database/users.go

--- database/users.go
+++ database/users.go
@@ -7,27 +7,6 @@ import (
 	"go.mongodb.org/mongo-driver/v2/bson"
 )
 
-func IsServedUser(userID int64) (bool, error) {
-	key := fmt.Sprintf("users:%d", userID)
-	if _, ok := cache.Load(key); ok {
-		return true, nil
-	}
-
-	ctx, cancel := context.WithTimeout(context.Background(), timeout)
-	defer cancel()
-
-	count, err := userDB.CountDocuments(ctx, bson.M{"user_id": userID})
-	if err != nil {
-		return false, err
-	}
-
-	if count > 0 {
-		cache.Store(key, true)
-	}
-
-	return count > 0, nil
-}
-
 func GetServedUsers() ([]int64, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	defer cancel()
@@ -54,44 +33,76 @@ func GetServedUsers() ([]int64, error) {
 	return userIDs, nil
 }
 
-func AddServedUser(userID int64) error {
-	key := fmt.Sprintf("users:%d", userID)
-	if _, ok := cache.Load(key); !ok {
-		exists, err := IsServedUser(userID)
-		if err != nil || exists {
-			return err
+func IsServedUser(userID int64) (bool, error) {
+	v, ok := cache.Load("users")
+	if ok {
+		users := v.([]int64)
+		for _, id := range users {
+			if id == userID {
+				return true, nil
+			}
 		}
 	}
 
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	count, err := userDB.CountDocuments(ctx, bson.M{"user_id": userID})
+	if err != nil {
+		return false, err
+	}
+	if count > 0 {
+		go func() {
+			val, _ := cache.LoadOrStore("users", []int64{})
+			users := val.([]int64)
+			users = append(users, userID)
+			cache.Store("users", users)
+		}()
+	}
+	return count > 0, nil
+}
+
+func AddServedUser(userID int64) error {
+	exists, err := IsServedUser(userID)
+	if err != nil || exists {
+		return err
+	}
 	go func() {
 		ctx, cancel := context.WithTimeout(context.Background(), timeout)
 		defer cancel()
 
 		_, err := userDB.InsertOne(ctx, bson.M{"user_id": userID})
 		if err == nil {
-			cache.Store(key, true)
+			val, _ := cache.LoadOrStore("users", []int64{})
+			users := val.([]int64)
+			users = append(users, userID)
+			cache.Store("users", users)
 		}
 	}()
 	return nil
 }
 
 func DeleteServedUser(userID int64) error {
-	key := fmt.Sprintf("users:%d", userID)
-	if _, ok := cache.Load(key); !ok {
-		exists, err := IsServedUser(userID)
-		if err != nil || !exists {
-			return err
+	exists, err := IsServedUser(userID)
+	if err != nil || !exists {
+		return err
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	_, err = userDB.DeleteOne(ctx, bson.M{"user_id": userID})
+	if err == nil {
+		val, _ := cache.Load("users")
+		if val != nil {
+			users := val.([]int64)
+			for i, id := range users {
+				if id == userID {
+					users = append(users[:i], users[i+1:]...)
+					break
+				}
+			}
+			cache.Store("users", users)
 		}
 	}
-
-	go func() {
-		ctx, cancel := context.WithTimeout(context.Background(), timeout)
-		defer cancel()
-
-		_, err := userDB.DeleteOne(ctx, bson.M{"user_id": userID})
-		if err == nil {
-			cache.Delete(key)
-		}
-	}()
-	return nil
+	return err
 }
