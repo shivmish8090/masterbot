d0bc3f23bcbe9e84062ef3f7b751ce778da8ea95
विवेक कुमार <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-04-07 20:55:42 +0530

    Create buttons.go

---
+++ utils/buttons.go
@@ -0,0 +1,52 @@
+package utils
+
+
+
+type Buttons struct {
+	RowWidth int
+	rows     [][]telegram.InlineKeyboardButton
+	tmpRow   []telegram.InlineKeyboardButton
+}
+
+func (b *Buttons) Inline(text, data string) telegram.InlineKeyboardButton {
+	return telegram.InlineKeyboardButton{
+		Text:         text,
+		CallbackData: data,
+	}
+}
+
+func (b *Buttons) Url(text, url string) telegram.InlineKeyboardButton {
+	return telegram.InlineKeyboardButton{
+		Text: text,
+		Url:  url,
+	}
+}
+
+// Add auto arrange buttons with RowWidth logic
+func (b *Buttons) Add(buttons ...telegram.InlineKeyboardButton) {
+	for _, btn := range buttons {
+		b.tmpRow = append(b.tmpRow, btn)
+		if len(b.tmpRow) == b.RowWidth {
+			b.rows = append(b.rows, b.tmpRow)
+			b.tmpRow = nil
+		}
+	}
+}
+
+// Row forcefully adds a new row (no RowWidth limit here)
+func (b *Buttons) Row(buttons ...telegram.InlineKeyboardButton) {
+	if len(b.tmpRow) > 0 {
+		b.rows = append(b.rows, b.tmpRow)
+		b.tmpRow = nil
+	}
+	b.rows = append(b.rows, buttons)
+}
+
+// Build final keyboard
+func (b *Buttons) Build() telegram.InlineKeyboardMarkup {
+	if len(b.tmpRow) > 0 {
+		b.rows = append(b.rows, b.tmpRow)
+		b.tmpRow = nil
+	}
+	return telegram.InlineKeyboardMarkup{InlineKeyboard: b.rows}
+}
