5988980355ae7eb5011dcec40d4923823c942d3f
विवेक कुमार <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-04-08 11:02:54 +0530

    Update buttons.go

--- utils/buttons.go
+++ utils/buttons.go
@@ -2,31 +2,27 @@ package utils
 
 import "github.com/PaulSonOfLars/gotgbot/v2"
 
-// Buttons is a smart inline button builder
-type Buttons struct {
+type Button struct {
 	RowWidth int
 	rows     [][]gotgbot.InlineKeyboardButton
 	tmpRow   []gotgbot.InlineKeyboardButton
 }
 
-// Inline creates a callback button
-func (b *Buttons) Inline(text, data string) gotgbot.InlineKeyboardButton {
+func (b *Button) Inline(text, data string) gotgbot.InlineKeyboardButton {
 	return gotgbot.InlineKeyboardButton{
 		Text:         text,
 		CallbackData: data,
 	}
 }
 
-// Url creates a url button
-func (b *Buttons) Url(text, url string) gotgbot.InlineKeyboardButton {
+func (b *Button) Url(text, url string) gotgbot.InlineKeyboardButton {
 	return gotgbot.InlineKeyboardButton{
 		Text: text,
 		Url:  url,
 	}
 }
 
-// Add adds buttons automatically using RowWidth rule
-func (b *Buttons) Add(buttons ...gotgbot.InlineKeyboardButton) {
+func (b *Button) Add(buttons ...gotgbot.InlineKeyboardButton) {
 	for _, btn := range buttons {
 		b.tmpRow = append(b.tmpRow, btn)
 		if len(b.tmpRow) == b.RowWidth {
@@ -36,8 +32,7 @@ func (b *Buttons) Add(buttons ...gotgbot.InlineKeyboardButton) {
 	}
 }
 
-// Row forces a new row with given buttons (ignores RowWidth)
-func (b *Buttons) Row(buttons ...gotgbot.InlineKeyboardButton) {
+func (b *Button) Row(buttons ...gotgbot.InlineKeyboardButton) {
 	if len(b.tmpRow) > 0 {
 		b.rows = append(b.rows, b.tmpRow)
 		b.tmpRow = nil
@@ -45,8 +40,7 @@ func (b *Buttons) Row(buttons ...gotgbot.InlineKeyboardButton) {
 	b.rows = append(b.rows, buttons)
 }
 
-// Build builds InlineKeyboardMarkup final structure
-func (b *Buttons) Build() gotgbot.InlineKeyboardMarkup {
+func (b *Button) Build() gotgbot.InlineKeyboardMarkup {
 	if len(b.tmpRow) > 0 {
 		b.rows = append(b.rows, b.tmpRow)
 		b.tmpRow = nil
