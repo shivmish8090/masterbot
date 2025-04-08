e54f5750471c5c918a287c0eb55121f17907ab66
विवेक कुमार <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-04-08 11:17:53 +0530

    update modules/echo.go

--- modules/echo.go
+++ modules/echo.go
@@ -56,7 +56,7 @@ func EcoHandler(b *gotgbot.Bot, ctx *ext.Context) error {
 	Message.Delete(b, nil)
 
 	keys := []string{"set-mode", "set-limit"}
-	_, res := utils.ParseFlags(keys, Message.Text)
+	_, res := config.ParseFlags(keys, Message.Text)
 	if res["set-mode"] != "" || res["set-limit"] != "" {
 		r := "Your settings were successfully updated:"
 		settings := &database.EchoSettings{ChatID: ChatId}
