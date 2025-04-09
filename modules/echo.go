2b08b8c4c78466e10b6514bcb2428fbc3b3f9a68
विवेक कुमार <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-04-09 15:11:42 +0530

    update modules/echo.go

--- modules/echo.go
+++ modules/echo.go
@@ -79,14 +79,12 @@ func EcoHandler(b *gotgbot.Bot, ctx *ext.Context) error {
 		
 	            return nil
 	        }
-	        } else {
+	    } else {
 	            
 	            
-	        }
-	        }
-	        
-	    }
-	    }
+	     }
+	     
+	}
 		member, err := ctx.EffectiveChat.GetMember(b, User.Id, nil)
 		if err != nil {
 			return err
