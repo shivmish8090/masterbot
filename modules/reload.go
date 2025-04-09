6564dfcb87a1b537d6d2ec910009f26956bdcbad
विवेक कुमार <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-04-09 19:50:08 +0530

    Update reload.go

--- modules/reload.go
+++ modules/reload.go
@@ -15,6 +15,39 @@ func ReloadHandler(b *gotgbot.Bot, ctx *ext.Context) error {
 	ChatId := ctx.EffectiveChat.Id
 
 	msg.Delete(b, nil)
-	b.SendMessage(ChatId, "Refreshing Cache of chat admins...", nil)
-	return nil
+	x, err := b.SendMessage(ChatId, "Refreshing Cache of chat admins...", nil)
+if err != nil {
+
+return err
+}
+
+                        chatmembers, e := b.GetChatAdministrators(ChatId, nil)
+                        if e != nil {
+
+
+x.EditTexg(b, fmt.Sprintf("Oops! Cache refresh failed — %v", e), nil)
+                                return e
+                        }
+
+                        var admins []int64
+                        for _, m := range chatmembers {
+                                status := m.GetStatus()
+                                if status == "administrator" || status == "creator" {
+                                        admins = append(admins, m.GetUser().Id)
+                                }
+                        }
+
+                        config.Cache.Store(cacheKey, admins)
+
+var text string
+                        if !config.Contains(admins, ctx.EffectiveUser.Id) {
+
+text = "✅ Successfully refreshed the cache of chat admins!"
+
+} else {
+
+text = "⚠️ Tried refreshing the admin cache... but oops! You're not an admin."
+}
+
+	x.EditTexg(b, text , nil)
 }
