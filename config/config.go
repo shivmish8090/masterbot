a9d60764248911ef9f6319dc8975b17e196d0335
विवेक कुमार <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-04-16 16:39:49 +0530

    Update config.go

--- config/config.go
+++ config/config.go
@@ -21,38 +21,34 @@ var (
 )
 
 func init() {
-	godotenv.Load()
-	StartTime = time.Now()
-	Token = Getenv("TOKEN", "8050656956:AAEPmjGKUwyMiN86p0lBnh3SPjnqXlPi9Ww", nil)
-	StartImage = Getenv(
-		"START_IMG_URL",
-		"https://telegra.ph/file/ba238ec5e542d8754cea7-dc1786aa23ae1224f2.jpg",
-		nil,
-	)
-	LoggerId = Getenv("LOGGER_ID", "-1002647107199", func(val string) int64 {
-		parsed, err := strconv.ParseInt(val, 10, 64)
-		if err != nil {
-			panic(fmt.Sprintf("Invalid LOGGER_ID: %v", err))
-		}
-		return parsed
-	})
-	MongoUri = Getenv("MONGO_DB_URI", "mongodb+srv://marin:marin69@cluster0.zxaf7uc.mongodb.net/?retryWrites=true&w=majority", nil)
-	OwnerId = Getenv("OWNER_ID", "7706682472", func(val string) int64 {
-		parsed, err := strconv.ParseInt(val, 10, 64)
-		if err != nil {
-			panic(fmt.Sprintf("Invalid OWNER_ID: %v", err))
-		}
-		return parsed
-	})
+        godotenv.Load()
+        StartTime = time.Now()
 
-	if Token == "" {
-		log.Panic("TOKEN environment variable is empty")
-	}
-	if MongoUri == "" {
-		log.Panic("MONGO_DB_URI environment variable is empty")
-	}
+        parseToInt64 := func(val string) int64 {
+                parsed, err := strconv.ParseInt(val, 10, 64)
+                if err != nil {
+                        panic(fmt.Sprintf("Error parsing int64: %v", err))
+                }
+                return parsed
+        }
+
+        Token = Getenv("TOKEN", "8050656956:AAEPmjGKUwyMiN86p0lBnh3SPjnqXlPi9Ww", nil)
+        StartImage = Getenv(
+                "START_IMG_URL",
+                "https://telegra.ph/file/ba238ec5e542d8754cea7-dc1786aa23ae1224f2.jpg",
+                nil,
+        )
+        LoggerId = Getenv("LOGGER_ID", "-1002647107199", parseToInt64)
+        MongoUri = Getenv("MONGO_DB_URI", "mongodb+srv://marin:marin69@cluster0.zxaf7uc.mongodb.net/?retryWrites=true&w=majority", nil)
+        OwnerId = Getenv("OWNER_ID", "7706682472", parseToInt64)
+
+        if Token == "" {
+                log.Panic("TOKEN environment variable is empty")
+        }
+        if MongoUri == "" {
+                log.Panic("MONGO_DB_URI environment variable is empty")
+        }
 }
-
 func Getenv[T any](key, defaultValue string, convert func(string) T) T {
 	value := defaultValue
 	if envValue, exists := os.LookupEnv(key); exists {
