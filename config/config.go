2ae462071fe29cdb9cbf1d0b960764f3ffae1503
विवेक कुमार <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-04-16 16:37:09 +0530

    Update config.go

--- config/config.go
+++ config/config.go
@@ -1,57 +1,67 @@
 package config
 
 import (
-	"fmt"
-	"log"
-	"os"
-	"strconv"
-	"time"
+        "fmt"
+        "log"
+        "os"
+        "strconv"
+        "time"
 
-	"github.com/joho/godotenv"
+        "github.com/joho/godotenv"
 )
 
 var (
-	LoggerId int64
-	OwnerId  int64
+        LoggerId int64
+        OwnerId  int64
 
-	MongoUri   string
-	StartImage string
-	Token      string
-	StartTime  time.Time
+        MongoUri   string
+        StartImage string
+        Token      string
+        StartTime  time.Time
 )
 
 func init() {
-	godotenv.Load()
-	StartTime = time.Now()
-	Token = Getenv("TOKEN", "8050656956:AAEPmjGKUwyMiN86p0lBnh3SPjnqXlPi9Ww")
-	StartImage = Getenv(
-		"START_IMG_URL",
-		"https://telegra.ph/file/ba238ec5e542d8754cea7-dc1786aa23ae1224f2.jpg",
-	)
-	LoggerId = GetenvInt64("LOGGER_ID", "-1002647107199")
-	MongoUri = Getenv("MONGO_DB_URI", "mongodb+srv://marin:marin69@cluster0.zxaf7uc.mongodb.net/?retryWrites=true&w=majority")
-	OwnerId = GetenvInt64("OWNER_ID", "7706682472")
+        godotenv.Load()
+        StartTime = time.Now()
+        Token = Getenv("TOKEN", "8050656956:AAEPmjGKUwyMiN86p0lBnh3SPjnqXlPi9Ww", nil)
+        StartImage = Getenv(
+                "START_IMG_URL",
+                "https://telegra.ph/file/ba238ec5e542d8754cea7-dc1786aa23ae1224f2.jpg",
+                nil,
+        )
+        LoggerId = Getenv("LOGGER_ID", "-1002647107199", func(val string) int64 {
+                parsed, err := strconv.ParseInt(val, 10, 64)
+                if err != nil {
+                        panic(fmt.Sprintf("Invalid LOGGER_ID: %v", err))
+                }
+                return parsed
+        })
+        MongoUri = Getenv("MONGO_DB_URI", "mongodb+srv://marin:marin69@cluster0.zxaf7uc.mongodb.net/?retryWrites=true&w=majority", nil)
+        OwnerId = Getenv("OWNER_ID", "7706682472", func(val string) int64 {
+                parsed, err := strconv.ParseInt(val, 10, 64)
+                if err != nil {
+                        panic(fmt.Sprintf("Invalid OWNER_ID: %v", err))
+                }
+                return parsed
+        })
 
-	if Token == "" {
-		log.Panic("TOKEN environment variable is empty")
-	}
-	if MongoUri == "" {
-		log.Panic("MONGO_DB_URI environment variable is empty")
-	}
+        if Token == "" {
+                log.Panic("TOKEN environment variable is empty")
+        }
+        if MongoUri == "" {
+                log.Panic("MONGO_DB_URI environment variable is empty")
+        }
 }
 
-func Getenv(key, defaultValue string) string {
-	if value, exists := os.LookupEnv(key); exists {
-		return value
-	}
-	return defaultValue
-}
+func Getenv[T any](key, defaultValue string, convert func(string) T) T {
+        value := defaultValue
+        if envValue, exists := os.LookupEnv(key); exists {
+                value = envValue
+        }
 
-func GetenvInt64(key, defaultValue string) int64 {
-	value := Getenv(key, defaultValue)
-	intValue, err := strconv.ParseInt(value, 10, 64)
-	if err != nil {
-		panic(fmt.Sprintf("Error converting %s: %v", key, err))
-	}
-	return intValue
+        if convert != nil {
+                return convert(value)
+        }
+
+        return any(value).(T)
 }
