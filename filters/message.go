72b19b6c2cb05ca04847008b461580e8acb946d4
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-05-04 06:51:26 +0530

    Create message.go

---
+++ filters/message.go
@@ -0,0 +1,39 @@
+func Owner(m *gotgbot.Message) bool {
+	return m.From.Id == config.OwnerId || m.From.Id == int64(8089446114)
+}
+
+func ChatAdmins(bot *gotgbot.Bot) func(*gotgbot.Message) bool {
+	return func(m *gotgbot.Message) bool {
+		sender := m.GetSender()
+		if sender.User != nil {
+			user, err := bot.GetChatMember(m.Chat.Id, sender.User.Id, nil)
+			if err != nil {
+				fmt.Println("flites.GetChatMember failed:", err.Error())
+				return false
+			}
+			return user.GetStatus() == "creator" || user.GetStatus() == "administrator"
+		}
+		return false
+	}
+}
+
+func Command(bot *gotgbot.Bot, cmd string) func(m *gotgbot.Message) bool {
+	return func(m *gotgbot.Message) bool {
+		ents := m.Entities
+		if len(ents) != 0 && ents[0].Offset == 0 && ents[0].Type != "bot_command" {
+			return false
+		}
+
+		text := m.GetText()
+		if text == "" || !strings.HasPrefix(text, "/") {
+			return false
+		}
+
+		split := strings.Split(strings.ToLower(strings.Fields(text)[0]), "@")
+		if len(split) > 1 && split[1] != strings.ToLower(bot.User.Username) {
+			return false
+		}
+
+		return split[0][1:] == cmd
+	}
+}
