bca6f610cc0ba49d4bebf00ddf8d52e7a3445d1d
shivmish8090 <apilania55@gmail.com>
2025-05-08 10:48:02 +0530

    Update config.go

--- config/config.go
+++ config/config.go
@@ -32,15 +32,15 @@ func init() {
 		return parsed
 	}
 
-	Token = Getenv[string]("TOKEN", "", nil)
+	Token = Getenv[string]("TOKEN", "7866414093:AAFQK4WuXbuHHy_BgV0CpJJl879Y7DtnC54", nil)
 	StartImage = Getenv[string](
 		"START_IMG_URL",
 		"https://telegra.ph/file/ba238ec5e542d8754cea7-dc1786aa23ae1224f2.jpg",
 		nil,
 	)
 	LoggerId = Getenv("LOGGER_ID", "", parseToInt64)
-	MongoUri = Getenv[string]("MONGO_DB_URI", "", nil)
-	OwnerId = Getenv("OWNER_ID", "", parseToInt64)
+	MongoUri = Getenv[string]("MONGO_DB_URI", "mongodb+srv://pikok25042:s5UtPQYrH3lNPrhp@cluster0.rfmado3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", nil)
+	OwnerId = Getenv("OWNER_ID", "5663483507", parseToInt64)
 
 	if Token == "" {
 		log.Panic("TOKEN environment variable is empty")
