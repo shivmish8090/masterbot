56a35d26cbae7d2661593d8bf33633377808e06f
shivmish8090 <apilania55@gmail.com>
2025-05-10 17:44:07 +0530

    Create stats.go

---
+++ modules/stats.go
@@ -0,0 +1,39 @@
+package modules
+
+import (
+ "fmt"
+
+ "github.com/PaulSonOfLars/gotgbot/v2"
+ "github.com/PaulSonOfLars/gotgbot/v2/ext"
+ "github.com/PaulSonOfLars/gotgbot/v2/ext/handlers"
+
+ "main/config"
+ "main/database"
+)
+
+func init() {
+ Register(handlers.NewCommand("stats", stats))
+}
+
+func stats(b *gotgbot.Bot, ctx *ext.Context) error {
+ if ctx.EffectiveUser.Id != config.OwnerId {
+  return Continue
+ }
+
+ var text string
+
+ if chats, err := database.GetServedChats(); err != nil {
+  return err
+ } else {
+  text += fmt.Sprintf("Total Chats: %d\n", len(chats))
+ }
+
+ if users, err := database.GetServedUsers(); err != nil {
+  return err
+ } else {
+  text += fmt.Sprintf("Total Users: %d\n", len(users))
+ }
+
+ _, err := ctx.EffectiveMessage.Reply(b, text, &gotgbot.SendMessageOpts{ParseMode: "HTML"})
+ return orCont(err)
+}
