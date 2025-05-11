619b2c2a28fa5452eb1334d2c019fc63dd526831
shivmish8090 <apilania55@gmail.com>
2025-05-11 07:35:02 +0530

    Update stats.go

--- modules/stats.go
+++ modules/stats.go
@@ -7,8 +7,8 @@ import (
  "github.com/PaulSonOfLars/gotgbot/v2/ext"
  "github.com/PaulSonOfLars/gotgbot/v2/ext/handlers"
 
- "main/config"
- "main/database"
+ "github.com/Vivekkumar-IN/EditguardianBot/config"
+ "github.com/Vivekkumar-IN/EditguardianBot/database"
 )
 
 func init() {
