0db269ef9460f50f7fe9a56bba2347190bef9f86
Viyom <151810083+ViYomX@users.noreply.github.com>
2025-05-21 07:22:54 +0530

    Create links.go

---
+++ modules/links.go
@@ -0,0 +1,22 @@
+package modules
+
+import (
+	"github.com/PaulSonOfLars/gotgbot/v2"
+	"github.com/PaulSonOfLars/gotgbot/v2/ext"
+)
+
+func DeleteLinkMessage(b *gotgbot.Bot, ctx *ext.Context) error {
+	m := ctx.EffectiveMessage
+
+	_, err := m.Delete(b, nil)
+	if err != nil {
+		return err
+	}
+	b.SendMessage(
+		m.Chat.Id,
+		"⚠️ Direct URLs aren't allowed.\nUse like <a href='https://t.me/durov'>this</a>",
+		&gotgbot.SendMessageOpts{ParseMode: "HTML", LinkPreviewOptions: &gotgbot.LinkPreviewOptions{IsDisabled: true}},
+	)
+
+	return Continue
+}
